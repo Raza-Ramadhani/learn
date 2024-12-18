f4688db495983b1f7c23f66bac777b0a35e09594
Raza <ramadhaniraza@gmail.com>
2024-12-18 14:50:29 +0100

    fixed number

--- app/ui/dezimal-form.tsx
+++ app/ui/dezimal-form.tsx
@@ -3,7 +3,7 @@ import Button from "./button"
 import Success from "./successMath"
 
 export default function DezimalForm() {
-    function mDecimalCheck(prevState:{randomNumber: Number, randomMultiplier: Number, correct:undefined|boolean},formData:FormData) {
+    function mDecimalCheck(prevState:{randomNumber: number, randomMultiplier: number, correct:undefined|boolean},formData:FormData) {
         const userNumber = formData.get('value')
         if (userNumber == null) {
             return prevState
@@ -11,7 +11,7 @@ export default function DezimalForm() {
         if(prevState.correct == true) {
             return reset(undefined)
         }
-        if (userNumber as unknown as Number == prevState.randomNumber) {
+        if (userNumber as unknown as number == prevState.randomNumber) {
             const newState = prevState
             prevState.correct = true
             return newState
@@ -27,8 +27,8 @@ export default function DezimalForm() {
         }
      const [state, submit] = useActionState(mDecimalCheck,
             {
-                randomNumber: Math.floor(Math.random() *2) as Number,
-                randomMultiplier: Math.floor(Math.random() * 99) as Number, 
+                randomNumber: Math.floor(Math.random() *2) as number,
+                randomMultiplier: Math.floor(Math.random() * 99) as number, 
                 correct: undefined as undefined|boolean,
             })
             const [isClient, setIsClient] = useState(false)
@@ -38,7 +38,7 @@ export default function DezimalForm() {
             }, [])
     return(
         <form action={submit}>
-                <h1 className="font-medium"><code className="bg-slate-300 rounded px-1">{state.randomNumber as number/ (state.randomMultiplier as number)} x {(state.randomMultiplier as number)}</code></h1>
+                <h1 className="font-medium"><code className="bg-slate-300 rounded px-1">{JSON.stringify(state.randomNumber/state.randomMultiplier)} x {JSON.stringify(state.randomMultiplier as number)}</code></h1>
                 <div className="text-5xl flex items-center bg-slate-100 p-2 rounded">
                         <input type='number' placeholder="Zahl"  step={0.000001} name="value" className="w-full bg-transparent appearance-none text-5xl font-meduum font-sans focus:outline-none"/><h1></h1>
                 </div>
